d7ad653375aae9b71c78dc017305cbb09211e289
vlad <svuitis@gmail.com>
2017-01-18 16:50:10 +0200

    Adding search parameters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
@@ -19,9 +7,25 @@
 //= require_tree .
 
 $(function () {
-    $( '#set_selector' ).multiselect( {
+    $( '#set_selector, #type_selector, #cost_selector' ).multiselect( {
+        nonSelectedText: 'All',
+        allSelectedText: 'All',
+        buttonWidth: '300px',
+        numberDisplayed: 1,
         onChange: function() {
-            $( 'html, body' ).animate( { scrollTop: 0 }, 1000 );
+            $( '#search_form' ).trigger( 'submit.rails' );
+        }
+    });
+
+    $( '#sub_type_selector' ).multiselect( {
+        nonSelectedText: 'All',
+        allSelectedText: 'All',
+        buttonWidth: '300px',
+        numberDisplayed: 2,
+        enableFiltering: true,
+        enableFullValueFiltering: true,
+        maxHeight: 400,
+        onChange: function() {
             $( '#search_form' ).trigger( 'submit.rails' );
         }
     });
