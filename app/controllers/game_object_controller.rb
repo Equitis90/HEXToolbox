ae21e4efed4d048960cb90665f4a018e073cfcb3
vlad <svuitis@gmail.com>
2017-01-26 10:36:38 +0200

    Preparation for production

--- app/controllers/game_object_controller.rb
+++ app/controllers/game_object_controller.rb
@@ -5,10 +5,12 @@ class GameObjectController < ApplicationController
     if @card.equipment_uuids != ''
       GameObject.where( uuid: @card.equipment_uuids.split( ', ' ) ).each do | equipment |
         @equipments[ equipment.uuid ] = {
-            name: equipment.name,
-            file_name: equipment.file_name,
-            text: equipment.text,
-            slot: equipment.sub_type
+          name: equipment.name,
+          file_name: equipment.file_name,
+          text: equipment.text.gsub('#VAR1_1#', '1')
+            .gsub(/\[([\(0-9\)]+|[A-Z\-^\]]+|[L0-9\]\[R0-9]+)\]/, '<span class=\0></span>')
+            .gsub( '#SELF#', @card.name ).gsub('#IGNORE_CHAIN#', ''),
+          slot: equipment.sub_type
         }
       end
     end
