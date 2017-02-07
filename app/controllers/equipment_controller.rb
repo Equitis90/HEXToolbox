21c791dcfbdc82520e592d52d8025687b7865758
vlad <svuitis@gmail.com>
2017-02-07 16:30:39 +0200

    Removing visitors counter with google analytics

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -2,9 +2,9 @@ class EquipmentController < ApplicationController
   include SearchEngine
 
   def list
-    equipments_query = "object_type = 'Equipment'"
+    equipment_query = "object_type = 'Equipment'"
     search_query, req_params = search
-    search_query = equipments_query + search_query
+    search_query = equipment_query + search_query
     @equipments = GameObject.where( search_query, req_params ).order( :name, :id, :set_number ).page( params[ :page ] ).per( 24 )
   end
   respond_to do |format|
